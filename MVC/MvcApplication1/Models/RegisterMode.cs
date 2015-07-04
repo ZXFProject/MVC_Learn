556ab2102c37ab7294e5add0ec6b29eec127509b
zxf <491167521@qq.com>
2015-07-04 17:56:07 +0800

    0704

---
+++ MVC/MvcApplication1/Models/RegisterMode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class RegisterMode
+    {
+        [Required]
+        [Display(Name="User name")]
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage="The {0} must be at least {2} ",MinimumLength=6)]
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
+        public string Password { get; set; }
+    }
+}
